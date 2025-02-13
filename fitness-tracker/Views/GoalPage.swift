c48314fae67afa6bc40cbc2a2c7b8a76b9eb9db3
mariagaric <maria.garic@hotmail.com>
2025-02-13 21:32:39 +0100

    Added navbar & view files

---
+++ fitness-tracker/Views/GoalPage.swift
@@ -0,0 +1,23 @@
+//
+//  GoalPage.swift
+//  fitness-tracker
+//
+//  Created by Maria Garic on 2025-02-13.
+//
+
+import SwiftUI
+
+struct GoalPage: View {
+    var body: some View {
+        VStack {
+            Text("Goals")
+                .font(.system(size: 16))
+                .padding(.top, 10)
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    GoalPage()
+}
---
+++ fitness-tracker/Views/LogSessionPage.swift
@@ -0,0 +1,23 @@
+//
+//  LogSession.swift
+//  fitness-tracker
+//
+//  Created by Maria Garic on 2025-02-13.
+//
+
+import SwiftUI
+
+struct LogSessionPage: View {
+    var body: some View {
+        VStack {
+            Text("Log Session")
+                .font(.system(size: 16))
+                .padding(.top, 10)
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    LogSessionPage()
+}
