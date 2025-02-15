8b05147a656074f68a94b573b218d2eb16faa1f5
Sternerr <43697087+Sternerr@users.noreply.github.com>
2025-02-15 14:42:22 +0100

    Add: APIService - Fetching data from API (#27)

---
+++ fitness-tracker/Service/APIService.swift
@@ -0,0 +1,46 @@
+//
+//  APIService.swift
+//  fitness-tracker
+//
+//  Created by Noah Sterner on 2025-02-13.
+//
+
+import SwiftData
+import Foundation
+
+struct Exercise: Decodable {
+    var name: String
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+    }
+}
+
+//Singleton Pattern
+class APIService {
+    static let shared = APIService()
+    private let baseURL = URL(string: "https://api.api-ninjas.com/v1")!
+    
+    private init() {}
+    
+    func fetchExercises() async -> [Exercise] {
+        let url = URL(string: "\(baseURL)/exercises")
+        var request = URLRequest(url: url!)
+        request.setValue(ENV.API_KEY, forHTTPHeaderField: "x-api-key")
+            
+        if let (data, _) = try? await URLSession.shared.data(for: request) {
+            return self.JSONfrom(data: data)
+        }
+        
+        return []
+    }
+    
+    private func JSONfrom(data: Data) -> [Exercise] {
+        do {
+            let decoder = JSONDecoder()
+            return try decoder.decode([Exercise].self, from: data)
+        } catch {
+            fatalError("")
+        }
+    }
+}
