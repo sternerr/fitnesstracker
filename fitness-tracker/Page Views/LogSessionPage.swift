a74bc72f0d39592e4e38d399578c8c0abdd082c3
mariagaric <maria.garic@hotmail.com>
2025-02-16 02:32:58 +0100

    Added logsession view

--- fitness-tracker/Page Views/LogSessionPage.swift
+++ fitness-tracker/Page Views/LogSessionPage.swift
@@ -8,16 +8,92 @@
 import SwiftUI
 
 struct LogSessionPage: View {
+    @State private var exercises: [String] = []
+    
     var body: some View {
         VStack {
-            Text("Log Session")
-                .font(.system(size: 16))
-                .padding(.top, 10)
-            Spacer()
+            HStack {
+                Button(action: {
+                    print ("Back pressed")
+                }) {
+                    Image(systemName: "chevron.left")
+                        .font(.title2)
+                        .foregroundColor(.primary70)
+                }
+                
+                Spacer()
+                
+                Text("Add Exercise")
+                    .font(.system(size: 16))
+                    .fontWeight(.bold)
+                
+                Spacer()
+                
+                Button(action: {
+                    print("Done pressed")
+                }) {
+                    Text("Done")
+                        .padding(.horizontal, 16)
+                        .padding(.vertical, 8)
+                        .background(Color.primary70)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                }
+            }
+            .padding()
+            
+            List {
+                ForEach(exercises, id:\.self) {exercise in
+                    ExerciseRow(exerciseName: exercise)
+                        .swipeActions {
+                            Button(role: .destructive) {
+                                if let index = exercises.firstIndex(of: exercise) {
+                                    exercises.remove(at: index)
+                                }
+                            } label: {
+                                Label("Delete", systemImage: "trash")
+                            }
+                        }
+                }
+                .onDelete(perform: deleteExercise)
+            }
+            
+            Button(action: {
+                exercises.append("New Exercise \(exercises.count + 1)")
+            }) {
+                Text ("Add Exercise")
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(Color.primary70)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+            .padding()
         }
+        .padding(.horizontal)
+    }
+    
+    func deleteExercise(at offsets: IndexSet) {
+        exercises.remove(atOffsets: offsets)
     }
 }
 
-#Preview {
-    LogSessionPage()
+struct ExerciseRow: View {
+    let exerciseName: String
+    
+    var body: some View {
+        HStack {
+            Text(exerciseName)
+                .padding()
+                .background(Color.gray.opacity(0.2))
+                .cornerRadius(8)
+            
+            Spacer()
+        }
+        .padding(.horizontal)
+    }
 }
+    
+    #Preview {
+        LogSessionPage()
+    }
