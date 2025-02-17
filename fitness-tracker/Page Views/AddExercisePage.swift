4e1d8f3c281259bf4d45e997579c1115fffe93ca
Noah <volpy2000@gmail.com>
2025-02-17 19:39:57 +0100

    Add: AddExercisePage

--- fitness-tracker/Page Views/AddExercisePage.swift
+++ fitness-tracker/Page Views/AddExercisePage.swift
@@ -8,42 +8,42 @@
 import SwiftUI
 
 struct AddExercisePage: View {
+    @Environment(\.presentationMode) var presentationMode
+    @Environment(\.dismiss) var dismiss
+    
     @Binding var exercises: [Exercise]
     @State private var text: String = ""
-    @Environment(\.presentationMode) var presentationMode
     
     var body: some View {
-        VStack {
-            HStack {
+        Container {
+            Block {
+                TopBar(text: "") {
+                    Image(systemName: "arrow.backward.square.fill")
+                        .resizable()
+                        .frame(width: 40.0, height: 40.0)
+                        .foregroundStyle(.secondarySurfaceContainer)
+                        .onTapGesture { dismiss() }
+                    
+                    
+                    TextField("Enter Exercise", text: $text) {}
+                        .padding(8)
+                        .font(.body)
+                        .cornerRadius(10)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(.secondarySurfaceContainer, lineWidth: 2)
+                        )
+                }
+                    
+                Button{
+                    exercises.append(Exercise(name: text))
+                    dismiss()
+                } label: {
+                    CustomButton(title: "Create Exercise")
+                }
+                        
                 Spacer()
             }
-            
-            TextField(text: $text) {}
-                .frame(height:70)
-                .border(Color.orange, width: 1)
-                .padding(.top, 50)
-            
-            Button(action: {
-                if !text.isEmpty {
-                    exercises.append(Exercise(name: text))
-                    text = ""
-                    presentationMode.wrappedValue.dismiss()
-                }
-            }) {
-                Text("Save")
-                    .frame(maxWidth: .infinity)
-                    .padding()
-                    .background(Color.primary70)
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
-            }
-            .padding(.top,30)
-            Spacer()
-
         }
     }
-}
-
-#Preview {
-    AddExercisePage()
 }
