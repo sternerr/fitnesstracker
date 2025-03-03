e1b64cbb1d09c4ac70728a50fe97de3734a73f25
Noah <volpy2000@gmail.com>
2025-03-03 13:18:32 +0100

    Added: WorkoutViewModel

---
+++ fitness-tracker/WorkoutViewModel.swift
@@ -0,0 +1,27 @@
+//
+//  WorkoutViewModel.swift
+//  fitness-tracker
+//
+//  Created by Noah Sterner on 2025-03-03.
+//
+
+import SwiftData
+import SwiftUI
+import Observation
+
+@Observable
+class WorkoutViewModel {
+    var modelContext: ModelContext? = nil
+    var workouts: [WorkoutModel] = []
+    
+    func fetchNewWorkout() {
+        self.workouts = (try? self.modelContext?.fetch(FetchDescriptor(predicate: #Predicate<WorkoutModel> {
+            $0.state == ""
+        }))) ?? []
+    }
+    
+    func addWorkout() {
+        self.modelContext?.insert(WorkoutModel())
+        self.fetchNewWorkout()
+    }
+}
--- fitness-tracker/fitness_trackerApp.swift
+++ fitness-tracker/fitness_trackerApp.swift
@@ -12,7 +12,7 @@ import SwiftData
 struct fitness_trackerApp: App {
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
-            Item.self,
+            WorkoutModel.self,
         ])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
 
